4431be1f748acaf79cedb7c463ce6bb8d83d79ae
hammad93 <ham.u@live.com>
2021-05-03 04:50:01 -0400

    output path

--- django/map/static/index.js
+++ django/map/static/index.js
@@ -19,6 +19,16 @@ document.getElementById('plot_button').onclick = function() {
     var sid = document.getElementById("inputGroupSelect04").value;
     var plot;
     $.get('/?type=plot&SID=' + sid, function(data, status){
-        console.log('test ' + sid + data)
+        console.log('test ' + sid + data);
+        plot = data;
     });
+    var i;
+    var path = []
+    for (i = 0; i < plot.length; i++) {
+        path.push(new google.maps.LatLng(plot["lat"][i], plot["lon"][i]))
+    }
+    var polyline = new google.maps.Polyline({path:path, strokeColor: "#FF0000", strokeOpacity: 1.0, strokeWeight: 2});
+    polyline.setMap(map);
+    map.setCenter(new google.maps.LatLng(plot["lat"][0], plot["lon"][0]), 6);
+
 }
