11738ba010d88f1518204425c3767b3e7af2ba7d
hammad93 <ham.u@live.com>
2021-05-03 00:50:04 -0400

    Add sidebar

--- django/map/map/urls.py
+++ django/map/map/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('hurricanes/', include('hurricane.urls')),
+    path('hurricane/', include('hurricane.urls')),
     path('admin/', admin.site.urls),
 ]
