e9ce83df7f72e73fb8bc9c4037bfdda6d3d0992b
hammad93 <ham.u@live.com>
2021-05-03 03:06:18 -0400

    Other design changes

--- django/map/static/index.js
+++ django/map/static/index.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(33.774640, -84.396417),
-      zoom:2,
+      zoom:7,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     document.getElementById("googleMap").style.width = window.innerWidth - 250 + "px";
