b4015285c7c649c2725ba154402de75311188839
hammad93 <ham.u@live.com>
2021-05-03 04:57:59 -0400

    output path

--- django/map/static/index.js
+++ django/map/static/index.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
 
 });
 
+var map;
 function myMap() {
     var mapProp= {
       center:new google.maps.LatLng(33.774640, -84.396417),
       zoom:7,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     document.getElementById("googleMap").style.width = window.innerWidth - 250 + "px";
 }
 
 document.getElementById('plot_button').onclick = function() {
     var sid = document.getElementById("inputGroupSelect04").value;
-    var plot;
-    $.get('/?type=plot&SID=' + sid, function(data, status){
+    var plot = $.get('/?type=plot&SID=' + sid, (data, status) => {
         console.log('test ' + sid + data);
-        plot = data;
+        return data;
     });
     var i;
     var path = []
