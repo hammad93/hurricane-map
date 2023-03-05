ee52b936e9a1af9eb430377fe412a68743a4f9ae
Hammad Usmani <ham.u@live.com>
2023-03-05 08:34:47 -0500

    Add OpenAPI configuration

--- django/map/map/urls.py
+++ django/map/map/urls.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hurricane API",
+      default_version='v1',
+      description="A collection of endpoints for hurricane research.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="husmani@fluids.ai"),
+      license=openapi.License(name="Creative Commons"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('hurricane.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
