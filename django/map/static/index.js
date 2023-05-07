5d5a7a9264c6ebc2186ce95db6feed02181a2738
Hammad Usmani <ham.u@live.com>
2023-05-06 22:43:52 -0400

    Update index.js

--- django/map/static/index.js
+++ django/map/static/index.js
@@ -67,7 +67,7 @@ function addMarkersAndLines(groupedData) {
     }
 }
 
-var map = L.map('map').setView([0, 0], 2);
+var map = L.map('map');
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
