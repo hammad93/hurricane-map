ab73dccef92837e4b4c84d0841c1f376b861fa8c
Hammad Usmani <ham.u@live.com>
2024-10-06 23:32:50 -0400

    Update index.js change to HTTPS

--- django/map/static/index.js
+++ django/map/static/index.js
@@ -384,7 +384,7 @@ function addGridLabels(viewer, minLat, maxLat, minLon, maxLon, latSpacing, lonSp
 }
 async function fetchLiveStorms() {
   try {
-      const response = await fetch('http://nfc.ai:1337/live-storms'); // Replace with your API endpoint
+      const response = await fetch('https://nfc.ai:1337/live-storms'); // Replace with your API endpoint
       if (!response.ok) {
           throw new Error(`HTTP error: ${response.status}`);
       }
@@ -603,7 +603,7 @@ function centerCameraOnLocation(viewer, latitude, longitude, zoomLevel = 1000000
 }
 async function fetchForecasts() {
   try {
-    const response = await fetch('http://nfc.ai:1337/forecasts');
+    const response = await fetch('https://nfc.ai:1337/forecasts');
 
     // Check if the request was successful
     if (!response.ok) {
